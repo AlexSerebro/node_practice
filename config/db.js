6b312cae90168d1c0a8af461fbd294de76547b36
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-08-11 20:03:13 +0300

    add console style

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const { connect } = require("mongoose");
+require("colors");
 
 const connectDB = async () => {
   try {
     const db = await connect(process.env.MONGODB_URI);
-    console.log(db);
+    console.log(
+      `MongoDB is connecteion on HOST ${db.connection.host} DB_name ${db.connection.name}`
+        .cyan.italic.underline
+    );
   } catch (error) {
-    console.log(error);
+    console.log(error.message.red);
   }
 };
 
